301451045911a756dd3fe498f66d01c11087356c
haiqal <muhammad.haiqal@agmo.group>
2024-05-09 22:47:31 +0800

    create layout

---
+++ src/app/layout/header/Header.jsx
@@ -0,0 +1,7 @@
+export function Header() {
+    return (
+        <>
+            <div className="text-bold text-center">Header</div>
+        </>
+    );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import App from './app/App.jsx';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Landing } from './app/pages/landing/Landing.jsx';
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <App />,
+        children: [
+            {
+                path: 'home',
+                element: <Landing />,
+            },
+        ],
     },
 ]);
 
