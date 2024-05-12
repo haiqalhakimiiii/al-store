96a1333b88220a7349d2d543cc7cd1f73783d3ad
haiqal <muhammad.haiqal@agmo.group>
2024-05-12 12:31:17 +0800

    fetch products

--- src/app/pages/Landing.jsx
+++ src/app/pages/Landing.jsx
@@ -1,6 +1,17 @@
+import { useEffect, useState } from 'react';
 
 export function Landing() {
-    console.log('without hook')
+    const [products, setProducts] = useState([]);
+
+    async function fetchProducts() {
+        const response = await fetch('https://fakestoreapi.com/products');
+        const products = await response.json();
+        console.log(products);
+    }
+
+    useEffect(() => {
+        fetchProducts();
+    }, []);
 
     return (
         <>
