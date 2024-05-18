ea7c898b69402580862ee03bfbbe5d135b237549
haiqalhakimiiii <haiqalhakimiiii@gmail.com>
2024-05-18 22:06:25 +0800

    change the loading loader color

--- src/app/components/Loading.jsx
+++ src/app/components/Loading.jsx
@@ -3,14 +3,14 @@ export function Loading({ count }) {
         let elements = [];
         for (let i = 0; i < count; i++) {
             elements.push(
-                <div className="w-50 h-80 shadow-md border hover:border-[#301934]">
+                <div className="w-50 h-80 shadow-md border">
                     <div className="animate-pulse flex flex-col justify-between bg-white h-full">
-                        <div className="w-full h-5/6 bg-slate-700"></div>
+                        <div className="w-full h-5/6 bg-[#301934]"></div>
                         <div className="w-full h-1/6 bg-gray-100 px-2 py-2">
-                            <div className="h-3 w-5/6 rounded bg-slate-700"></div>
+                            <div className="h-3 w-5/6 rounded bg-[#301934]"></div>
                             <div className="flex justify-between mt-3">
-                                <div className="h-3 w-1/3 rounded bg-slate-700"></div>
-                                <div className="h-3 w-1/6 rounded bg-slate-700"></div>
+                                <div className="h-3 w-1/3 rounded bg-[#301934]"></div>
+                                <div className="h-3 w-1/6 rounded bg-[#301934]"></div>
                             </div>
                         </div>
                     </div>
