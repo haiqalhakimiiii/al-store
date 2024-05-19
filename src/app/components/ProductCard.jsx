1f045d4b5f143ada228cc2d92e087a096f8b0b9d
haiqalhakimiiii <haiqalhakimiiii@gmail.com>
2024-05-19 16:23:41 +0800

    create productCard component

---
+++ src/app/components/ProductCard.jsx
@@ -0,0 +1,32 @@
+import { Link } from 'react-router-dom';
+
+export function ProductCard({ products }) {
+    return (
+        <>
+            <div className="grid grid-cols-6 gap-4">
+                {products.map((product) => (
+                    <Link key={product.id} to={`product-detail/${product.id}`}>
+                        <div className="cursor-pointer">
+                            <div className="w-50 h-80 shadow-md border hover:border-[#301934]">
+                                <div className="flex flex-col justify-between h-full bg-white">
+                                    <img src={product.image} className="w-50 h-60 object-contain" alt="Product Image" />
+
+                                    <div className="px-1 py-1 bg-gray-100">
+                                        <p className="truncate">{product.title}</p>
+                                        <div className="flex justify-between">
+                                            <div className="text-[#669243]">RM{product.price}</div>
+                                            <div className="flex gap-1">
+                                                <div>{product.rating.rate}</div>
+                                                <div>({product.rating.count})</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </Link>
+                ))}
+            </div>
+        </>
+    );
+}
--- src/app/components/ProductList.jsx
+++ src/app/components/ProductList.jsx
@@ -1,7 +1,7 @@
 import { useCallback, useEffect, useState } from 'react';
 import { Sort } from './Sort';
 import { Loading } from './Loading';
-import { Link } from 'react-router-dom';
+import { ProductCard } from './ProductCard';
 
 export const baseUrl = 'https://fakestoreapi.com';
 
@@ -48,34 +48,7 @@ export function ProductList() {
                     </div>
                 ) : (
                     <>
-                        <div className="grid grid-cols-6 gap-4">
-                            {products.map((product) => (
-                                <Link key={product.id} to={`product-detail/${product.id}`}>
-                                    <div className="cursor-pointer">
-                                        <div className="w-50 h-80 shadow-md border hover:border-[#301934]">
-                                            <div className="flex flex-col justify-between h-full bg-white">
-                                                <img
-                                                    src={product.image}
-                                                    className="w-50 h-60 object-contain"
-                                                    alt="Product Image"
-                                                />
-
-                                                <div className="px-1 py-1 bg-gray-100">
-                                                    <p className="truncate">{product.title}</p>
-                                                    <div className="flex justify-between">
-                                                        <div className="text-[#669243]">RM{product.price}</div>
-                                                        <div className="flex gap-1">
-                                                            <div>{product.rating.rate}</div>
-                                                            <div>({product.rating.count})</div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </Link>
-                            ))}
-                        </div>
+                        <ProductCard products={products} />
                         {productCount < 20 && (
                             <div className="text-center pt-5">
                                 <button
